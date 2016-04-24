7f84253227bed6ce4516545007548eb3173935a0
Rik Bruil <rikbruil@users.noreply.github.com>
2016-04-24 22:03:02 +0200

    Fix header

--- src/Credentials/CredentialsInterface.php
+++ src/Credentials/CredentialsInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Jira Client library.
+ *
+ * (c) Rik Bruil <rikbruil@users.noreply.github.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE file.
+ */
+
 namespace Rb\JiraClient\Credentials;
 
 interface CredentialsInterface
